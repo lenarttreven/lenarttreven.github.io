ff0763867de3a44ede71a72be6cb22de34dcc773
lenarttreven <lenart.treven44@gmail.com>
2025-09-07 13:49:36 +0000

    Deploying to gh-pages from @ lenarttreven/lenarttreven.github.io@46461a29b1c2b05481ae18a94837bf7888db971a 🚀

--- assets/js/giscus-setup.js
+++ assets/js/giscus-setup.js
@@ -18,7 +18,7 @@ function determineGiscusTheme() {
 
   let giscusAttributes = {
     src: "https://giscus.app/client.js",
-    "data-repo": "lenarttreven/temporary_webpage",
+    "data-repo": "lenarttreven/lenarttreven.github.io",
     "data-repo-id": "",
     "data-category": "Comments",
     "data-category-id": "",
